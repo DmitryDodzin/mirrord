496d5699b6cc977bb23f995a28f07f49ab321581
Dmitry Dodzin <d.dodzin@gmail.com>
2022-09-06 21:24:54 +0300

    Revert "create codec with actix-codec for preview communication"
    This reverts commit 9caaa23f3825af84a7527b21bf3817b7b4d04f69.

--- mirrord-preview/src/lib.rs
+++ mirrord-preview/src/lib.rs
@@ -1,6 +1,5 @@
 #[cfg(feature = "client")]
 pub mod client;
-pub mod codec;
 pub mod connection;
 pub mod filter;
 pub mod proxy;
@@ -14,6 +13,3 @@ pub struct PreviewConfig {
     pub deny_ports: filter::FilterPorts,
     pub listen_for_updates: bool,
 }
-
-pub type ClientCodec = codec::BincodeCodec<proxy::ProxiedResponse, proxy::ProxiedRequest>;
-pub type ServerCodec = codec::BincodeCodec<proxy::ProxiedRequest, proxy::ProxiedResponse>;
