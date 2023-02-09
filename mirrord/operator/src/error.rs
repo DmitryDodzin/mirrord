445f2c8527ca2e609122dfdab25c2d6ec9378da4
Dmitry Dodzin <d.dodzin@gmail.com>
2023-02-09 14:43:58 +0200

    Encode and Decode

--- mirrord/operator/src/error.rs
+++ mirrord/operator/src/error.rs
@@ -1,6 +1,9 @@
+#[cfg(feature = "client")]
+use bincode::{Decode, Encode};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
+#[cfg_attr(feature = "client", derive(Decode, Encode))]
 pub enum OperatorError {
     #[error("Unable to lock port {0} for target {1}, currently locked by {2}")]
     LockedPort(u16, String, String),
